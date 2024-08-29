e7b713d6f9a93700b0f730043d4569b0a621afc2
Z-radi30 <zakaria1992radi@gmail.com>
2024-08-30 00:00:59 +0100

    App.jsx doesn't contain: ["/blog/:id"]

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home'; // Assuming you have a Home component
 import About from './components/About'; // Assuming you have an About component
 import Profile from './components/Profile'; // Assuming you have a Profile component
-import BlogPost from './components/BlogPost'; // The BlogPost component you just created
+import BlogPost from './components/BlogPost'; // The BlogPost component to handle dynamic routes
 
 function App() {
   return (
@@ -13,17 +13,17 @@ function App() {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/post/1">First Blog Post</Link></li>
-          <li><Link to="/post/2">Second Blog Post</Link></li>
-          <li><Link to="/post/3">Third Blog Post</Link></li>
+          <li><Link to="/blog/1">Blog Post 1</Link></li>
+          <li><Link to="/blog/2">Blog Post 2</Link></li>
+          <li><Link to="/blog/3">Blog Post 3</Link></li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/post/:postId" element={<BlogPost />} /> {/* Dynamic Route */}
+        <Route path="/profile/*" element={<Profile />} /> {/* Profile component with nested routes */}
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic route for blog posts */}
       </Routes>
     </Router>
   );
