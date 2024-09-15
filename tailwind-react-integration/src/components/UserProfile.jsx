fbbaab287429544ed2e6956b00f403db7e03bd17
Z-radi30 <zakaria1992radi@gmail.com>
2024-09-15 23:06:00 +0100

    1. Styling a Provided React Component with Tailwind CSS final fix

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,20 +1,15 @@
-
-
 function UserProfile() {
-    return (
-        <div className="hover:shadow-xl transition-shadow duration-300 ease-in-out p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto">
-        <img 
-            src="https://via.placeholder.com/150"
-            alt="User"
-            className="transition-transform duration-300 ease-in-out hover:scale-110 sm:w-24 sm:h-24 md:w-36  md:h-36 rounded-full mx-auto mb-4"
-        />
-         <h1 className="hover:text-blue-500 transition-colors duration-300 ease-in-out ttext-lg md:text-xl sm:text-xl font-bold text-center">John Doe</h1>
-         <p className="mt-2 text-sm sm:text-base text-gray-600 text-center">
-            Developer at Example Co. Loves to write code and explore new technologies.
-         </p>
-      </div>
-    );
-  }
-  
-  export default UserProfile;
+  return (
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <img 
+          src="https://via.placeholder.com/150"
+          alt="User"
+          className="w-36 h-36 rounded-full mx-auto"
+      />
+      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <p  className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    </div>
+  );
+}
 
+export default UserProfile;
